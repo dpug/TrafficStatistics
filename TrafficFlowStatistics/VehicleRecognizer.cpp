5b87741830c661dfbade644171182ef6b1a48d09
dpug <jpugachev@hotmail.com>
2014-08-07 22:05:00 -0400

    got rid of gray shadows

--- TrafficFlowStatistics/VehicleRecognizer.cpp
+++ TrafficFlowStatistics/VehicleRecognizer.cpp
@@ -10,11 +10,8 @@ bool VehicleRecognizer::ProcessNextFrame()
 		MOG2(frame, fgMaskMOG);
 	else
 		MOG(frame, fgMaskMOG);
-	//Get the frame number and write it on the current frame
-	rectangle(frame, Point(10, 2), Point(100, 20), Scalar(255,255,255), -1);
-	ss << capture.get(CV_CAP_PROP_POS_FRAMES);
-	string frameNumberString = ss.str();
-	putText(frame, frameNumberString.c_str(), Point(15, 15), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0));
+	//Get rid of gray colors and convert to binary image using threshold operation
+	threshold(fgMaskMOG, fgMaskMOG, 200, 255, THRESH_BINARY);
 }
 
 Mat VehicleRecognizer::getNextFrameMat()
@@ -37,11 +34,6 @@ VehicleRecognizer::VehicleRecognizer (string SourceVideoFileName, bool UseMOG2Su
 {
 	this->mbUseMOG2 = UseMOG2Substraction;
 	this->SourceFileName = SourceVideoFileName;
-	//if (this->mbUseMOG2)
-	//	pMOG = new BackgroundSubtractorMOG2();
-	//else
-	//	pMOG = new BackgroundSubtractorMOG();
-	
 	capture = VideoCapture(this->SourceFileName);
 	if (!capture.isOpened())
 		throw std::exception("Can't open source filename!");
@@ -51,7 +43,6 @@ VehicleRecognizer::VehicleRecognizer(void)
 {
 	this->mbUseMOG2 = false;
 	this->SourceFileName = nullptr;
-	//pMOG = new BackgroundSubtractorMOG();
 	capture = VideoCapture(this->SourceFileName);
 	if (!capture.isOpened())
 		throw std::exception("Can't open source filename!");
--- TrafficFlowStatistics/main.cpp
+++ TrafficFlowStatistics/main.cpp
@@ -4,33 +4,9 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	//Old method
-	//IplImage *currentFrame = 0;
-
-	//try
-	//{
-	//	cvNamedWindow("output");
-	//	VehicleRecognizer VehicleRec("source.mp4");
-	//	for(;;)
-	//	{
-	//		VehicleRec.ProcessNextFrame();
-	//		currentFrame = VehicleRec.getNextFrame();
-	//		if (!currentFrame)
-	//			break;
-	//		cvShowImage("output", currentFrame);
-	//		cvWaitKey(24);
-	//	}
-	//}
-	//catch(exception &exc)
-	//{
-	//	cout << exc.what() << endl;
-	//}
 	int keyboard = 0;
-
-	//cvReleaseImage(&currentFrame);
 	namedWindow("Frame");
-	////namedWindow("FG Mask MOG");
-	VehicleRecognizer VehicleRec("video.avi", false);
+	VehicleRecognizer VehicleRec("source.mp4", true);
 	while((char)keyboard != 'q' && (char)keyboard != 27)
 	{
 		if (!VehicleRec.ProcessNextFrame())
